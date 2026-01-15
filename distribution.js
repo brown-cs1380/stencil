334e1da0b6d3535c068ce5a598346862c68cf766
cs1380 <cs1380@brown.edu>
2026-01-15 18:19:31 +0100

    v0.2.20
    - Bump version (7df39b3)
    - Put distributionLib init at the start (9472a08)
    - Improve distribution initialization (df762df)
    - Use the global distribution object (a134342)
    - Bump version (1436d5e)
    - Move git push to the end (3b83c10)
    - Make spawn/stop return error object if unimplemented (485ffe5)
    - Fix M3 skip logic (bfa52cc)
    - Add more test early exits (b8acd47)
    - Fix type comment (feb1d0f)
    - Access service object from global distribution object (a3bf256)
    - Include }) in the stencil (53105be)
    - Actually invoke distribution in the student tests (c0a5f64)
    - Add pointer to container (ca793a6)
    - Improve component signature comments (8aa0fe7)
    - Remove stencil Dockerfile (dd5c1c6)
    - Remove redundant parts in scripts (c4e4989)

--- distribution.js
+++ distribution.js
@@ -9,35 +9,40 @@ const log = require('./distribution/util/log.js');
  * @param {Node} [config]
  */
 function bootstrap(config) {
+    // This loads the reference implementation in case you want to selectively override parts of your implementation with it (e.g., extra credit ypu are not doing or missing functionality from previous milestones).
+    // This needs to run before the rest of the code so that the globalThis.distribution object is populated correctly.
+    // @ts-ignore Optional dependency for reference implementation.
+    const distributionLib = require('@brown-ds/distribution')(config);
+    distributionLib; // To avoid unused variable warning
+  const distribution = {};
   // @ts-ignore This is the first time globalThis.distribution is being initialized, so the object does not have all the necessary properties.
-  globalThis.distribution = {};
-  globalThis.distribution.util = require('./distribution/util/util.js');
+  globalThis.distribution = distribution;
+  distribution.util = require('./distribution/util/util.js');
   // @ts-ignore node.server is lazily initialized.
-  globalThis.distribution.node = require('./distribution/local/node.js');
-  globalThis.distribution.local = require('./distribution/local/local.js');
+  distribution.node = require('./distribution/local/node.js');
+  distribution.local = require('./distribution/local/local.js');
 
   if (config) {
-    globalThis.distribution.node.config = config;
+    distribution.node.config = config;
   }
 
-  for (const [key, service] of Object.entries(globalThis.distribution.local)) {
-    globalThis.distribution.local.routes.put(service, key, () => {});
+  for (const [key, service] of Object.entries(distribution.local)) {
+    distribution.local.routes.put(service, key, () => {});
   }
 
   const {setup} = require('./distribution/all/all.js');
-  globalThis.distribution.all = setup({gid: 'all'});
+  distribution.all = setup({gid: 'all'});
 
   /* Overrides when missing functionality from previous milestone or extra credit is needed */
 
   // For M3, when missing RPC, its path through routes, and status.{spawn, stop}
   /* __start_M3_solution__
-  const distributionLib = require('@brown-ds/distribution')(config);
-  globalThis.distribution.util.wire.createRPC = distributionLib.util.wire.createRPC;
-  globalThis.distribution.local.routes.get = distributionLib.local.routes.get;
-  globalThis.distribution.local.status.spawn = distributionLib.local.status.spawn;
-  globalThis.distribution.local.status.stop = distributionLib.local.status.stop;
+  distribution.util.wire.createRPC = distributionLib.util.wire.createRPC;
+  distribution.local.routes.get = distributionLib.local.routes.get;
+  distribution.local.status.spawn = distributionLib.local.status.spawn;
+  distribution.local.status.stop = distributionLib.local.status.stop;
   __end_M3_solution__ */
-  return globalThis.distribution;
+  return distribution;
 }
 
 /*
@@ -46,7 +51,7 @@ function bootstrap(config) {
   1. The reference implementation from the library @brown-ds/distribution
   2. Your own, local implementation
 
-  Which one to be used by the tests is determined by the value of the property "useLibrary" in the package.json file.
+  Which one to be used by the tests is determined by the "useLibrary" value in the package.json file.
 */
 // @ts-ignore JSON import resolved at runtime.
 const {useLibrary} = require('./package.json');
